442693a7751d0a8756b14b80ef1098f730ffc8b9
Przemek <przemek.bzowka@gmail.com>
2019-04-07 19:28:51 +0200

    Updates

---
+++ precache-manifest.8ab78592308ec5bde56f8ec4de74f7aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "85aeb9e314abe0b887e11e4776905cc1",
+    "url": "/react_kanban_app/static/media/search.85aeb9e3.svg"
+  },
+  {
+    "revision": "ddc2583aba427d936605402e3f3ca577",
+    "url": "/react_kanban_app/static/media/close.ddc2583a.svg"
+  },
+  {
+    "revision": "ab2ff5e757e7972063cd",
+    "url": "/react_kanban_app/static/js/runtime~main.ab2ff5e7.js"
+  },
+  {
+    "revision": "eb026e0e048eef7ae090",
+    "url": "/react_kanban_app/static/js/main.eb026e0e.chunk.js"
+  },
+  {
+    "revision": "2eb6d22236dcc384c924",
+    "url": "/react_kanban_app/static/js/2.2eb6d222.chunk.js"
+  },
+  {
+    "revision": "eb026e0e048eef7ae090",
+    "url": "/react_kanban_app/static/css/main.e646b071.chunk.css"
+  },
+  {
+    "revision": "3e025248159f62337c16aabbe4ca2cfe",
+    "url": "/react_kanban_app/index.html"
+  }
+];
